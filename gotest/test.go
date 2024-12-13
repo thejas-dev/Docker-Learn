69cc467083ac86bb3e5c9aee570adb2234668dbc
Thejas Hari <thejas.hari@ei.study>
2024-12-13 15:45:03 +0530

    java&go added and image uploaded to hub

---
+++ gotest/test.go
@@ -0,0 +1,9 @@
+// Write a simple go program
+package main
+
+import "fmt"
+
+func main() {
+	name := "Thejas Hari"
+	fmt.Println("Hello, World! " + name)
+}
---
+++ javatest/hello.java
@@ -0,0 +1,7 @@
+
+public class hello {
+    public static void main(String[] args) {
+        String name = "Thejas Hari";
+        System.out.println("Hello, World! " + name);
+    }
+}
